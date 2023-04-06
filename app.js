b44ef4057b41271f3465c477d76575efa58318ec
Srijan Verma <srijanv9@gmail.com>
2023-04-06 12:43:29 +0530

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
 import entryRouter from "./routes/Entry.js";
+import cors from "cors";
+
 export const app = express();
 //using middleware
 app.use(express.json());
@@ -9,7 +11,13 @@ app.use(express.json());
 // app.use("/api/v1/users", userRouter);
 // app.use("/api/v1/task", taskRouter);
 app.use("/api", entryRouter);
-
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST"],
+    credentials: true,
+  })
+);
 config({
   path: "./data/config.env",
 });
